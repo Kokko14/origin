91673a305a75cfef6f9648a9e2c31e6d5b5f0b16
PEPEXD12345 <dmendezr@tecmd.edu.co>
2025-03-04 08:16:57 -0500

    Delete calculadora.js

--- calculadora.js
+++
@@ -1,6 +0,0 @@
-function suma()  {
-    let num1 = parseFloat(document.getElementById("num1").value)
-    let num2 = parseFloat(document.getElementById("num2").value)
-    let suma = num1 + num2; 
-    document.getElementById("resultado").innerText = isNaN(suma) ? "porfavor ingrese numeros validos": suma;
-}
